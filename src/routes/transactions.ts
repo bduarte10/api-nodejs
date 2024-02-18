8843edbd89e5b6f5b66642bf83dfbe22ff5ac4ae
Bruno <duartebruno2208@gmail.com>
2024-02-18 20:40:35 -0300

    feat: get summary and get transaction by id

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -3,6 +3,27 @@ import { knex } from '../database'
 import { z } from 'zod'
 
 export async function transactionsRoutes(app: FastifyInstance) {
+  app.get('/', async () => {
+    const transactions = await knex('transactions').select()
+    return { transactions }
+  })
+
+  app.get('/:id', async (req) => {
+    const getTransactionParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+    const { id } = getTransactionParamsSchema.parse(req.params)
+    const transaction = await knex('transactions').where('id', id).first()
+    return { transaction }
+  })
+
+  app.get('/summary', async () => {
+    const summary = await knex('transactions')
+      .sum('amount', { as: 'amount' })
+      .first()
+    return { summary }
+  })
+
   app.post('/', async (req, res) => {
     const createTransactionBodySchema = z.object({
       title: z.string(),
